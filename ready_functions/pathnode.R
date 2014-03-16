2f97ceecdc097de3f3a9a2c4c93e88d89310247f
David <duchenedavid@gmail.com>
2014-03-16 18:49:21 +1100

    Minor change to pathnode.R

--- ready_functions/pathnode.R
+++ ready_functions/pathnode.R
@@ -1,6 +1,6 @@
 
 pathnode <- function(phylo, tipsonly = T){
-	 library(phangorn)
+	 require(phangorn)
     di.tr <- dist.nodes(phylo)
     root.tr <- phylo$edge[, 1][!(phylo$edge[, 1] %in% phylo$edge[, 2])][1]
     tr.depth <- max(di.tr[as.numeric(colnames(di.tr)) == root.tr, ])
@@ -12,6 +12,6 @@ pathnode <- function(phylo, tipsonly = T){
 		roottotippath <- di.tr[as.numeric(rownames(di.tr)) == root.tr, ]
 		nodesinpath <- sapply(1:length(phylo$tip.label), function(x) length(Ancestors(phylo, x)))
 	}
-	plot(roottotippath, nodesinpath, xlab = "Root-to-tip path length", ylab = "Number of parent nodes")
+	plot(roottotippath, nodesinpath, xlab = "Root-to-tip path length", ylab = "Number of parent nodes", pch = 20)
 	return(list(roottotippath = roottotippath, nodesinpath = nodesinpath))
 }
