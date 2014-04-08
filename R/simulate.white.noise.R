0512edff3eb12be60d436ad385ef542c7423cf17
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-04-08 21:48:50 +1000

    added white noise model

--- R/simulate.white.noise.R
+++ R/simulate.white.noise.R
@@ -1,10 +1,8 @@
 simulate.white.noise <-
-function(tree, params = list(mean.log = -3.9, sd.log = 0.1)){
-    mean.log <- params$mean.log
-    sd.log <- params$sd.log
+function(tree, params = NULL){
     data.matrix <- get.tree.data.matrix(tree)
-    branch.noise <- sd.log / (data.matrix[, 7] / mean(data.matrix[, 7]))
-    branch.rates <- sapply(branch.noise, function(x) rlnorm(1, mean.log, branch.noise))
+    means.rates <- data.matrix[, 7] / sum(data.matrix[, 7])
+    branch.rates <- sapply(means.rates, function(x) log(rlnorm(1, x, x)))
     data.matrix[, 5] <- branch.rates
     data.matrix[, 6] <- data.matrix[, 5] * data.matrix[, 7]
     tree$edge.length <- data.matrix[, 6]
