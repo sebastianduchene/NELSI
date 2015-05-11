4ad94e469b9231df19910ef658864c780e26856f
Sebastian Duchene <sebastian.duchene@sydney.edu.au>
2015-05-11 17:44:24 +1000

    added experimental function to simulate rates with beta distribution

--- experimental/simulate.uncor.beta.R
+++ experimental/simulate.uncor.beta.R
@@ -1,9 +1,7 @@
-simulate.uncor.lnorm <-
-function(tree, params = list(mean.log = -3.9, sd.log = 0.1)){
-    mean.log <- params$mean.log
-    sd.log <- params$sd.log
+simulate.uncor.beta <-
+function(tree, params = list(shape1 = 0.4, shape2 = 0.4, scale_param = 150, center_param = 0.0015)){
     data.matrix <- get.tree.data.matrix(tree)
-    branch.rates <- rlnorm(n = length(tree$edge.length), meanlog = mean.log, sdlog = sd.log)
+    branch.rates <- (rbeta(n = length(tree$edge.length), shape1 = params$shape1, shape2 = params$shape2) / params$scale_param) + params$center_param
     data.matrix[, 5] <- branch.rates
     data.matrix[, 6] <- data.matrix[, 5] * data.matrix[, 7]
     tree$edge.length <- data.matrix[, 6]
