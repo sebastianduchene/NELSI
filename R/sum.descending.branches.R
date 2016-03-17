a327e7436f821b9115dacf3ddbf1a7302b979ce3
Sebastian Duchene <sebastian.duchene@sydney.edu.au>
2016-03-17 15:15:22 +1100

    updated function to sum descending branch lengths

---
+++ R/sum.descending.branches.R
@@ -0,0 +1,7 @@
+sum_descending_branches <- function(tr, target_node){
+    tips_descendants <- match(tips(tr, target_node), tr$tip.label)
+    descending_nodes <- unique(unlist(lapply(tips_descendants, function(x) nodepath(tr, from = target_node, to = x))))
+    descending_nodes <- descending_nodes[descending_nodes != target_node]
+    branch_lengths <- tr$edge.length[tr$edge[, 2] %in% descending_nodes]
+    sum(branch_lengths)
+}
