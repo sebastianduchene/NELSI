17c0a962367bece7858eb40a1b85cde10db588f4
Sebastian Duchene <sebastian.duchene@sydney.edu.au>
2016-05-12 14:52:29 +1000

    topodist

---
+++ R/dist.topo.normalised.R
@@ -0,0 +1,12 @@
+topo_dist_standardised <- function(tr1, tr2, nrand=100){
+    dists_sim <- vector()
+    for(i in 1:nrand){
+        tr_temp <- tr2
+        tr_temp$tip.label <- sample(tr2$tip.label)
+        dists_sim[i] <- dist.topo(tr1, tr_temp)
+    }
+    max_dist <- max(dists_sim)
+    return(dist.topo(tr1, tr2) / max_dist)
+}
+
+
