4c256b6522882608be099190372fdcf37c71cb21
Sebastian Duchene <sduchene@5310-b127601.local>
2017-10-18 13:40:31 +1100

    update nodes

--- R/allnode.times.R
+++ R/allnode.times.R
@@ -1,11 +1,14 @@
 allnode.times <-
-function(phylo, tipsonly = FALSE){
+function(phylo, tipsonly = FALSE, reverse = T){
     di.phylo <- dist.nodes(phylo)
     root.phylo <- phylo$edge[, 1][!(phylo$edge[, 1] %in% phylo$edge[, 2])][1]
     phylo.depth <- max(di.phylo[as.numeric(colnames(di.phylo)) == root.phylo, ])
     node.times <- phylo.depth - di.phylo[as.numeric(rownames(di.phylo)) == root.phylo, ]
     if(tipsonly == TRUE){
     	node.times <- phylo.depth - di.phylo[as.numeric(rownames(di.phylo)) == root.phylo, 1:length(phylo$tip.label)]
+	if(reverse){
+		node.times <- abs(node.times - max(node.times))
+	}
     }
     return(node.times)
 }
