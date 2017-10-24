32ab6ede45808e3c156f5d33b42ca9a13d85433d
Sebastian Duchene <sduchene@5310-b127601.mobility.unimelb.net.au>
2017-10-24 11:52:48 +1100

    updated lsd dates file

--- R/make_lsd_date_file.R
+++ R/make_lsd_date_file.R
@@ -1,13 +1,6 @@
-make_lsd_date_file <- function(phylodata, outfile = 'outfile.date'){
-    if(class(phylodata) == 'DNAbin'){
-        taxa_names <- rownames(phylodata)
-    }else if(class(phylodata) == 'phylo'){
-        taxa_names <- phylodata$tip.label
-    }
-
-    dates <- sapply(taxa_names, function(x) gsub('.+_', '', x), USE.NAMES = F)
-    lines <- paste0(taxa_names, ' ', dates, collapse = '\n')
-    cat(length(taxa_names), '\n', file = outfile)
-    cat(lines, file = outfile, append = T)
-    print(paste('Dates file saved in ', outfile))
+make.lsd.dates <- function(tr, grep.sep = '.+_', outfile = 'out.date'){
+  dates <- gsub(grep.sep, '', tr$tip.label)
+  dates.vector <- paste(tr$tip.label, dates, sep = '\t')
+  cat(length(tr$tip.label), '\n', file = outfile)
+  cat(dates.vector, sep = '\n', file = outfile, append = T)
 }
